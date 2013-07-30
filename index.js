35c902eafe9676580fe46aa8bcfe1ebd8526648e
Kenneth Kan <kenhkan@gmail.com>
2013-07-30 19:51:54 -0400

    Close #1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /*
- * This file can be used for general library features that are exposed as CommonJS modules
- * that the components then utilize
+ * This file can be used for general library features of flow.
+ *
+ * The library features can be made available as CommonJS modules that the
+ * components in this project utilize.
  */
